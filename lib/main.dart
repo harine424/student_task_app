09a06e74e7c45f3bdce1b7b1b636bd6e1655de90
harine424 <harine424@gmail.com>
2026-06-14 19:44:33 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Student Task App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF1E3A8A)),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(body: Center(child: Text('App Initialized!'))),
+    );
+  }
+}
