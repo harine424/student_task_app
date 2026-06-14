dabc12645b081855899b63302a97f315d2d220b7
harine424 <harine424@gmail.com>
2026-06-14 21:15:59 +0800

    Set Up the API Connection

---
+++ lib/services/api_path.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/foundation.dart';
+
+class ApiPath {
+  static String get baseUrl {
+    if (kIsWeb) {
+      return "http://localhost/taskapp_server/api";
+    }
+
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return "http://10.0.2.2/taskapp_server/api";
+      default:
+        return "http://localhost/taskapp_server/api";
+    }
+  }
+
+  static String endpoint(String fileName) {
+    return "$baseUrl/$fileName";
+  }
+}
